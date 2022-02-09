24e3c853c5192c70db388f82cb89ea02d930f7cc
Tobias Buschor <tobias.buschor@shwups.ch>
2022-02-09 21:50:08 +0100

    initial

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # aspect-ratio.el
+
+Since we can't use the css property aspect-ratio yet (Safari) we have an element here that brings a good implementation of aspect-ratio with it
+
+# Features
+- No `overflow:hidden`
+- No `position:relative` or `position:absolute`
+- `min-height` and `max-height` wins over the aspect-ratio
+- Use the class '-body' to fill the available height
+- CSS only fallback
+- If the content is heighter than calculated by aspect-ratio, the content-height wins
+- Performant and responsive, because the aspect ratio is generated with css
+- Note: to make the element work with border and padding, the element has a forced `box-sizing:content-box`
+
+# Ussage
+
+```html
+<link rel=stylesheet href="https://cdn.jsdelivr.net/gh/u1ui/aspect-ratio.el@x.x.x/aspect-ratio.min.css">
+<script src="https://cdn.jsdelivr.net/gh/u1ui/aspect-ratio.el@x.x.x/aspect-ratio.min.js" type=module></script>
+
+<u1-aspect-ratio style="--ratio:1; max-height:100vh; min-height:200px; border:2px solid red">
+    <div class=-body>
+        Some Content
+    </div>
+</u1-aspect-ratio>
+```
---
+++ aspect-ratio.js
@@ -0,0 +1,52 @@
+// todo add privet fields
+/* element */
+class AspectRatio extends HTMLElement {
+    constructor() {
+        super();
+        let shadowRoot = this.attachShadow({mode:'open'});
+        shadowRoot.innerHTML = `
+        <style>
+            :host {
+                display: block;
+                box-sizing: content-box !important;
+            }
+            :host > .wrapper {
+                display:grid;
+                min-height:inherit;
+                max-height:inherit;
+
+                flex: 1 1 auto; /* if host is a flex-container */
+            }
+            :host > .wrapper > slot {
+                grid-area: 1 / 1 / 1 / 1;
+                display:flex;
+                flex-direction:column;
+            }
+            :host > .wrapper > .spacer {
+                grid-area: 1 / 1 / 1 / 1;
+                min-height:inherit;
+                max-height:inherit;
+                overflow:hidden;
+                display:block;
+            }
+            :host > .wrapper > .spacer::after {
+                content: '';
+                display: block;
+                box-sizing: border-box;
+                padding-bottom: calc( var(--u1-ratio) * 100% );
+                height: 0;
+                width: 0;
+            }
+            ::slotted(.-body) {
+                flex:1 1 auto;
+            }
+        </style>
+        <div class=wrapper>
+            <div class=spacer></div>
+            <slot></slot>
+        </div>
+        `;
+    }
+}
+
+customElements.define('u1-aspect-ratio', AspectRatio)
